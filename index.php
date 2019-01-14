e49afefca3d229eefc9639837587e10461a87290
krzychna <kkrzys17@gmail.com>
2019-01-14 10:47:23 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+function getStructure($path){
+    if (file_exists($path) and is_dir($path)) {
+        $structure = array("path"=>$path, "type"=>"dir", "components"=>[]);
+        $files = scandir($path);
+        $files = array_diff($files,array(".",".."));
+        foreach ($files as $key => $file) {
+            if (is_dir("$path/$file")) {
+                array_push($structure["components"],getStructure("$path/$file"));
+            }else if (is_file("$path/$file")) {
+                array_push($structure["components"], array("path"=> $file, "type"=>"file"));
+            }
+        }
+    }
+    return $structure;
+}
+
+
+$structureJSON = json_encode(getStructure(__DIR__));
+print_r($strictureJSON);
+?>
